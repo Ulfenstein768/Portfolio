8ab15540bfcd19d4331b2b117795a46b10a1a0c2
Ulfurerulfur <utbodvarsson@gmail.com>
2022-10-26 19:06:52 +0000

    Update readme file

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -35,7 +35,6 @@ export default function Home() {
 
   return (
     <div>
-      {/* NEED TO FIX WIDTH FOR THE FORM. */}
       <div className="w-h h-full">
         <form
           onSubmit={handleSubmit(onSubmitForm)}
